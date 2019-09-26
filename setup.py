7ebf6f2475069ea6eec3ef1abd393c076a61204e
Metin Senturk <metinsenturk@me.com>
2019-09-26 18:35:09 -0400

    addional args added to setup again.

--- setup.py
+++ setup.py
@@ -7,14 +7,23 @@ setup(
     name='flat_table',
     version='1.0.0',
     packages=find_packages(),
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
-    description='A broader implementation of json_normalize()',
+    license='MIT',
     author='Metin Senturk',
     author_email='metinsenturk@me.com',
     url='https://github.com/metinsenturk/flat_table'
     keywords=['pandas', 'json_normalize', 'dict_to_columns', 'flatten']
+    description='A broader implementation of json_normalize()',
     long_description=long_description,
+    long_description_content_type="text/markdown"
     install_requires=[
         'pandas'
-    ]
+    ],
+    python_requires='>=3.6'
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: Scientific/Engineering :: Information Analysis"
+    ],
 )
